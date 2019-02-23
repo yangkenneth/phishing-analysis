27b3fd112d9d4cdf13556064f74f0fd22b6e47e0
Ciy-e <kiahadjkhan@gmail.com>
2019-02-23 15:24:07 -0500

    set up Scrapy and added an example crawler that returns all outbound links on a page. Updated README with information about scrapy.

--- README.md
+++ README.md
@@ -7,9 +7,18 @@ Phishing Sites: Build a seed list of phising domains that are updated daily, and
 Mongo DB
 `sudo apt-get install mongodb`
 
+Scrapy
+`pip install scrapy`
+
+Scrapyd (for hosting the spiders on the local machine)
+`pip install scrapyd`
+
 Pymongo 
 `pip install pymongo`
 
+### Example: to run Scrapy and output all outbound links found on a page to a csv file
+
+`scrapy runspider crawler.py -o links.csv -t csv`
 
 **Start Script:**\
 Under User Home Directory:
--- src/crawler.py
+++ src/crawler.py
@@ -1,8 +1,38 @@
-from database import Database
-from models.post import Post
+#from database import Database
+#from models.post import Post
+#Database.initialize('fullstack', 'phishing')
+
+import scrapy
+from scrapy.item import Item, Field
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+class Link(scrapy.Item):
+    url_from = Field()
+    url_to = Field()
 
 
-Database.initialize('fullstack', 'phishing')
+# example Scrapy crawler
+class PhishingSiteSpider(scrapy.Spider):
+    name = 'phishing_site_spider'
+    start_urls = [
+        'http://www.gatech.edu'
+    ]
+
+    rules = Rule(LinkExtractor(canonicalize=True, unique=True), callback='parse', follow=False)
+    
+    # all outbound links
+    def parse(self, response):
+        items = []
+        links = LinkExtractor(canonicalize=True, unique=True).extract_links(response)
+
+        for ref in links:
+            item = Link()
+            item['url_from'] = response.url
+            item['url_to'] = ref.url
+            items.append(item)
+        
+        return items
 
 # POST
 # post = Post('ID', "WEBSITE_ID", "WEBSITE_URL", "CONTENT", "DATE")
